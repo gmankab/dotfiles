ac2445ff33912aedeeff32d0d0f918bcf3a19986
gmankab <gmankab@gmail.com>
2024-04-09 17:28:55 +0400

    [u] fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,13 +3,16 @@ set fish_cursor_default block
 set fish_cursor_insert line
 set fish_cursor_replace_one underscore
 set fish_cursor_visual block
+set fish_greeting
+set -Ux EDITOR nvim
+set -g fish_sequence_key_delay_ms 100
+bind -M insert -m default jk cancel repaint-mode
 bind y fish_clipboard_copy
 bind p fish_clipboard_paste
 alias pc='pacman --noconfirm'
 alias ya='yay --noconfirm'
-alias hyfetch='python -m hyfetch'
-alias sshuttle='python -m sshuttle'
-alias dlp='distrobox enter arch -- python -m yt_dlp'
 fish_add_path -m ~/.local/bin
-fish_add_path -m ~/.pyenv/versions/3.12.1/bin
+if test -d ~/.pyenv
+  fish_add_path -m ~/.pyenv/versions/*/bin
+end
 
