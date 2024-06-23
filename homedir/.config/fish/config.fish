22c09c21482868ea83f2c647164cf81a942d2d2e
gmankab <gmankab@gmail.com>
2024-06-23 19:37:44 +0400

    [u] fish config

--- homedir/.config/fish/config.fish
+++ homedir/.config/fish/config.fish
@@ -17,3 +17,18 @@ if test -d ~/.pyenv
   fish_add_path -m ~/.pyenv/versions/*/bin
 end
 
+
+# xdg
+set XDG_CONFIG_HOME $HOME/.config
+set XDG_CACHE_HOME $HOME/.cache
+set XDG_STATE_HOME $HOME/.local/state
+set XDG_DATA_HOME $HOME/.local/share
+set XDG_DATA_DIRS /usr/local/share:/usr/share:/var/lib/flatpak/exports/share:$XDG_DATA_HOME/flatpak/exports/share
+set XDG_CONFIG_DIRS /etc/xdg
+set PYTHONPYCACHEPREFIX $XDG_CACHE_HOME/python
+set PYTHONUSERBASE $XDG_DATA_HOME/python
+set PYTHON_HISTORY $XDG_STATE_HOME/python/history
+set RUFF_CACHE_DIR $XDG_CACHE_HOME/ruff
+set CARGO_HOME $XDG_DATA_HOME/cargo
+set WINEPREFIX $XDG_DATA_HOME/wineprefixes/default
+
