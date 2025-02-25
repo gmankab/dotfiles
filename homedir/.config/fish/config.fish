6b790b51fe09ada043c1929e5b0275c26b7d846d
gmanka <gmankab@gmail.com>
2025-02-25 17:32:57 +0400

    [u] fish config

--- homedir/.config/fish/config.fish
+++ homedir/.config/fish/config.fish
@@ -1,21 +1,16 @@
 fish_vi_key_bindings
 fish_add_path -m ~/.local/bin
-fish_add_path -m ~/.cargo/bin
 
 bind y fish_clipboard_copy
 bind p fish_clipboard_paste
 bind -M default n 'down-or-search'
 bind -M default e 'up-or-search'
 
-alias pc='pacman --noconfirm'
-alias ya='yay --noconfirm'
-
 set fish_cursor_default block
 set fish_cursor_insert line
 set fish_cursor_replace_one underscore
 set fish_cursor_visual block
 set fish_greeting
-set -Ux EDITOR nvim
 
 # xdg
 set XDG_CONFIG_HOME $HOME/.config
@@ -28,11 +23,17 @@ set PYTHONPYCACHEPREFIX $XDG_CACHE_HOME/python
 set PYTHONUSERBASE $XDG_DATA_HOME/python
 set PYTHON_HISTORY $XDG_STATE_HOME/python/history
 set RUFF_CACHE_DIR $XDG_CACHE_HOME/ruff
-set CARGO_HOME $XDG_DATA_HOME/cargo
-set WINEPREFIX $XDG_DATA_HOME/wineprefixes/default
 
 if type -q zoxide
     zoxide init fish | source
     alias cd=__zoxide_z
+end
+
+if type -q nvim
+    set -x EDITOR nvim
+else if type -q vim
+    set -x EDITOR vim
+else if type -q vi
+    set -x EDITOR vi
 end
 
